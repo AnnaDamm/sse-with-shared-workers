d283e600e6fdde21728398715ef3694f27c9b255
Anna Damm <793921+AnnaDamm@users.noreply.github.com>
2021-08-17 16:44:27 +0200

    [main] change while loop

--- public/server.php
+++ public/server.php
@@ -2,25 +2,41 @@
 
 declare(strict_types=1);
 
+header('Access-Control-Allow-Origin: *'); // only for example
+
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
 ob_end_clean();
 
 $id = getallheaders()['Last-Event-Id'] ?? 0;
+$ping = (bool) $id % 2;
 
 while (true) {
     $id++;
+    $ping = !$ping;
     echo <<<TEXT
         id: $id
-        data: ping
-        
-        event: pong
-        data: pong
+        data: id: $id
 
 
         TEXT;
 
+    if ($ping) {
+        echo <<<PING
+        event: ping
+        data: ping?
+
+
+        PING;
+    } else {
+        echo <<<PONG
+        event: pong
+        data: pong!
+
+
+        PONG;
+    }
     sleep(1);
 
     // Break the loop if the client aborted the connection (closed the page)
